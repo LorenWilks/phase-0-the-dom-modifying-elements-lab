01eb3eec07be7f6b5bf4e81b5887613ca624aba9
Loren Wilks <Loren.wilks@gmail.com>
2022-06-09 10:44:09 -0600

    commit tree f3c6607ec8195209200c89bad26054d69812c76d

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code here!
+main.remove();
+const newHeader = document.createElement('h1', { is: 'h1#victory'});
+newHeader.id = 'victory';
+newHeader.textContent = 'Loren is the champion';
